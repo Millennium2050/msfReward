c0ffe35f708cc35b30fab4c2555b803ffb99377e
Hasan Othman§ <michleanglo@yahoo.com>
2024-03-23 18:55:15 +0100

    firefox driver added

--- msfReward/Utitly.cs
+++ msfReward/Utitly.cs
@@ -6,21 +6,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium.DevTools.V120.DeviceOrientation;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
 
 namespace msfReward
 {
     public class Utitly
     {
         private readonly EdgeOptions _options;
-        private WebDriver _driver;
+        //private readonly FirefoxOptions _options;
+        private IWebDriver _driver;
+        
 
+        //// Optional: Set the profile directory if needed
+        //// options.Profile = new FirefoxProfileManager().GetProfile("profile_name");
+
+        //// Create FirefoxDriver instance with options
+        //IWebDriver driver = new FirefoxDriver(options);
         public Utitly()
         {
             _options = new EdgeOptions();
-            _options.AddArgument("user-data-dir=D:\\Edge\\User Data");
+
+            //_options = new FirefoxOptions();
+
+            //_options.AddArgument("user-data-dir=D:\\Edge\\User Data");
             _options.AddAdditionalEdgeOption("useAutomationExtension", false);
             _options.AddExcludedArgument("enable-automation");
+
             _driver = new EdgeDriver(_options);
+            //_options.Profile = new FirefoxProfileManager().GetProfile("C:\\Users\\hasan\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\5urz0jxy.default-release");
+            //var anaheimService = ChromeDriverService.CreateDefaultService(@"c:\drivers", "msedgedriver.exe");
+            //var anaheimOptions = new ChromeOptions
+            //{
+            //    BinaryLocation = @"C:\Users\hasan\AppData\Local\Microsoft\Edge SxS\Application\msedge.exe"
+            //};
+            ////anaheimOptions.AddArgument("--headless");
+            //_driver = new ChromeDriver(anaheimService, anaheimOptions);
+            //_driver = new FirefoxDriver(_options);
         }
 
         public void StartBrowser() {
@@ -43,7 +66,7 @@ namespace msfReward
               
                     searchInput.Submit();
 
-                    Thread.Sleep(100000);
+                    Thread.Sleep(10000);
 
                     _driver.Close();
                 }
@@ -55,7 +78,8 @@ namespace msfReward
                 Thread.Sleep(10000);
                 try
                 {
-                    _driver = new EdgeDriver(_options); 
+                    _driver = new EdgeDriver(_options);
+                    //_driver = new FirefoxDriver(_options);
                 }
                 catch (Exception e)
                 {
@@ -92,6 +116,37 @@ namespace msfReward
             "Azure Functions deployment steps",
             "Dockerizing .NET applications"
         };
+
+
+            // Array of random search keywords related to IT fields
+             cryptoKeywords = [
+                        "web development tools",
+            "cloud computing services",
+            "data analysis techniques",
+            "network security best practices",
+            "software development methodologies",
+            "machine learning algorithms",
+            "front-end frameworks comparison",
+            "backend development frameworks",
+            "IT project management tools",
+            "database management systems",
+                // Add more relevant keywords as needed
+           ];
+
+            dotnetKeywords = [
+                        "artificial intelligence applications",
+            "cybersecurity threats analysis",
+            "agile software development",
+            "full-stack development tutorials",
+            "DevOps practices and tools",
+            "IT infrastructure management",
+            "blockchain technology overview",
+            "ethical hacking techniques",
+            "user experience design principles",
+            "IoT (Internet of Things) devices",
+            // Add more relevant keywords as needed
+        ];
+
 
             Random rand = new Random();
             string[] selectedKeywords = rand.NextDouble() < 0.5 ? cryptoKeywords : dotnetKeywords;
